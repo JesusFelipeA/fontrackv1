f6b558baeaa5ba6b51193a134ee2705546721af0
JesusFelipeA <al222310418@gmail.com>
2025-05-22 15:39:30 -0600

    sexto commit

--- resources/views/index_materiales.blade.php
+++ resources/views/index_materiales.blade.php
@@ -4,6 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="favicon.ico">
+    <link rel="icon" type="image/png" sizes="32x32" href="../">
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
     <title>Lista de Materiales - FontTrack</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
@@ -54,6 +57,7 @@
             background-color: #D1784C;
             transform: scale(1.05);
         }
+
         /* Mejoramos la interfaz de los botones dentro de la tabla */
         .btn-info {
             background-color: #88C0D0;
